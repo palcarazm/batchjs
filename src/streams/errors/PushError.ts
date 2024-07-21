0a2faaf2325be6accb8cc018015fa11a84d8d48f
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2024-07-21 15:47:53 +0200

    refactor: modify common files tree

--- src/streams/errors/PushError.ts
+++ src/streams/errors/PushError.ts
@@ -1,4 +1,4 @@
-import { BatchJSError } from "../../common/errors/BatchJSError";
+import { BatchJSError } from "../../common/errors/_index";
 
 export class PushError extends BatchJSError{
     constructor(){

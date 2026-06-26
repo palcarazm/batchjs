1f2928d782f11f26d30440c111062c13547bbaaa
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2026-06-26 10:17:18 +0200

    feat: enhance documentation with meta tags, favicon, and logo assets

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,23 @@ export default defineConfig({
     title: "BatchJS",
     description: "A lightweight, stream-based batch processing library for Node.js",
     base: "/batchjs/",
+
+    head: [
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/batchjs/favicon.ico' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/batchjs/favicon.png' }],
+    ['meta', { name: 'author', content: 'https://github.com/palcarazm' }],
+    ['meta', { name: 'robots', content: 'index, follow' }],
+    ['meta', { name: 'revisit-after', content: '1 month' }],
+    ['meta', { property: 'og:description', content: 'A dependencies free batch processing framework for NodeJS based on streams to build robust, composable pipelines with Jobs, Steps, and Streams.' }],
+    ['meta', { property: 'og:url', content: 'https://palcarazm.github.io/batchjs' }],
+    ['meta', { property: 'og:image', content: 'https://palcarazm.github.io/batchjs/card.png' }],
+    ['meta', { property: 'og:image:width', content: '728' }],
+    ['meta', { property: 'og:image:height', content: '364' }],
+  ],
+
     themeConfig: {
+        logo: "/logo.png",
+        siteTitle: "",
         search: {
             provider: "local",
         },
